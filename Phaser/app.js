c241873b3ac3f1be67df789614fa76db8d4f9c03
SoulOS25 <sean.tongs@gmail.com>
2017-03-11 10:58:49 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -9,7 +9,9 @@ function preload() {
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE)
 	game.add.sprite(0,0,'sky');
-	
+	platforms=game.add.group();
+	platforms.enableBody=true;
+	var ground=platforms.create(0,game.world.height-50,'ground')
 }
 
 function update() {
