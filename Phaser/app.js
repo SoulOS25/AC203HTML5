b482fd047fa7c638eeee23e6fead81a21d991018
SoulOS25 <sean.tongs@gmail.com>
2017-03-11 11:12:39 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -16,8 +16,7 @@ function create() {
 	ground.body.immovable=true;
 	var ledge=platforms.create(-150,250,'ground');
 	ledge.body.immovable=true;
-	ledge.body.immovable=true;
-	var ledge=platforms.create(-150,250,'ground');
+	var ledge=platforms.create(400,400,'ground');
 	ledge.body.immovable=true;
 }
 }
