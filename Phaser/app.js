a911a1a9dbf9603faa5381326cdd5752f4233eaa
SoulOS25 <sean.tongs@gmail.com>
2017-03-18 12:08:20 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -62,8 +62,10 @@ function update() {
 		star.kill();
 		score+=1;
 		scoreText.text="Score:"+score;
-		var star=stars.create(Math.random()*750,20,'star');
+		for(var i=0;i<10;i++){
+			var star=stars.create(Math.random()*750,20,'star');
 		star.body.gravity.y=200;
 		star.body.bounce.y=0.6+Math.random()*0.3;
+		}	
 	}
 }
