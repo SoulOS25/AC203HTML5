847501a0e824dd315ad5bfa3b0de6d5e5fe2420c
SoulOS25 <sean.tongs@gmail.com>
2017-03-18 10:59:40 +0800

    phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -29,7 +29,7 @@ function create() {
 	stars=game.add.group();
 	stars.enableBody=true;
 	for (var i=0;i<12;i++){
-		var star=star.create(60*i,20,'star');
+		var star=stars.create(60*i,20,'star');
 		star.body.gravity.y=200;
 		star.body.bounce.y=0.6+Math.random()*0.3;
 	}
